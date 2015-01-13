854d089d077bf514c5d47b7957fc154017bf4c7a
Zhang Xue <yuki@deep194.siat.ac.cn>
2015-01-13 08:34:31 +0800

    update install_Anaconda.sh

--- install_Anaconda.sh
+++ install_Anaconda.sh
@@ -3,5 +3,5 @@ cd
 wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh
 installer="Anaconda-2.1.0-Linux-x86_64.sh"
 chmod +x "$installer"
-sudo "./$installer"
+sudo bash "./$installer"
 rm "$installer"
