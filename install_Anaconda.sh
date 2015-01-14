305bf867e5e040d9327531622485d499c2fa745f
Zhang Xue <yuki@deep194.siat.ac.cn>
2015-01-14 14:53:02 +0800

    update some installers

--- install_Anaconda.sh
+++ install_Anaconda.sh
@@ -4,4 +4,6 @@ wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcd
 installer="Anaconda-2.1.0-Linux-x86_64.sh"
 chmod +x "$installer"
 sudo bash "./$installer"
+sudo mv ~/anaconda/lib/libm.so ~/anaconda/lib/libm.so.tmp
+sudo mv ~/anaconda/lib/libm.so.6 ~/anaconda/lib/libm.so.6.tmp
 rm "$installer"
--- install_caffe.sh
+++ install_caffe.sh
@@ -6,9 +6,7 @@ cp caffe.Makefile.config "${caffe_root_dir}/Makefile.config"
 read -p "Please modify Makefile.config according to your setup"
 vim "${caffe_root_dir}/Makefile.config"
 cd "$caffe_root_dir"
-sudo mv ~/anaconda/lib/libm.so ~/anaconda/lib/libm.so.tmp
-sudo mv ~/anaconda/lib/libm.so.6 ~/anaconda/lib/libm.so.6.tmp
-read -p "how many threads you want to use to compile caffe" core
+read -p "how many threads you want to use to compile caffe: " core
 make all -j$core
 make test -j$core
 make pycaffe -j$core
